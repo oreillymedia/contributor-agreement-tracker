252afe03ee6a6c72e35529f5fdac41fb4885e8ac
Steve Klise <sklise@oreilly.com>
2013-08-14 15:38:46 -0400

    Convert app to modular style and include atlas_assets files

---
+++ assets/javascripts/atlas_assets.js
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require underscore
+//= require handlebars.runtime
+//= require string
+//= require backbone
+//= require bootstrap
+//= require keypress
+//= require pusher
+//= require atlas_assets_setup
