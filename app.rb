9cf3e96880e4ca0fa195dfa036140610366371b4
Andrew Odewahn <odewahn@oreilly.com>
2013-08-15 12:39:15 -0400

    base commit

--- app.rb
+++ app.rb
@@ -7,6 +7,20 @@ class App < Sinatra::Base
   set :root, File.dirname(__FILE__)
 
   get '/' do
-    erb :hello
+    erb :index
   end
+  
+  get '/contributor_agreement' do
+    erb :contributor_agreement
+  end 
+
+  post '/confirm' do
+    @email = params[:email]
+    erb :confirm
+  end 
+  
+  get '/faq' do
+    erb :faq
+  end
+  
 end
